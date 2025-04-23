d13a2a445b85d8a22464253c100c2749179abb78
Captain Jack <Chavdaravi4u@gmail.com>
2025-04-23 18:40:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
-auth_users = [7890781002,6373737837]
+api_id = "22484191"
+api_hash = "5add27078ec7008f4dd93311a9cd72af"
+bot_token = "7004755641:AAGOiJRcNSydy6B6QrrMHH6oMynGl8_04hw"
+auth_users = [6969805021]
